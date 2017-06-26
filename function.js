c264d90d75f3180826dc05c085687bb25038be9e
Yuxin Zhang <taylorzhangyx@gmail.com>
2017-06-26 16:38:35 -0700

    basic functions

---
+++ function.js
@@ -0,0 +1,5 @@
+var getSum = function (num1, num2) {
+    return num1 + num2;
+};
+var theSum1 = getSum(5, 2);
+document.write("5 + 2 = " + theSum1 + "<br />");
---
+++ function.ts
@@ -0,0 +1,7 @@
+var getSum = function(num1: number, num2: number): number{
+  return num1 + num2;
+}
+
+var theSum1: number = getSum(5, 2);
+
+document.write("5 + 2 = " + theSum1 + "<br />");
