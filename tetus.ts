ad3ab60e7c9033b562e60b7f4a81b28136cc964d
Yuxin Zhang <taylorzhangyx@gmail.com>
2017-06-26 14:11:50 -0700

    add main pages

---
+++ tetus.ts
@@ -0,0 +1,6 @@
+var myName: string = "Derek";
+var myAge: number = 41;
+var canVote: boolean = true;
+var veriable: any = 2;
+
+document.getElementById("tsStuff").innerHTML = "My Name is " + myName;
