7b6bd3805cd1992f7b15431648404274b17e57cb
Yuxin Zhang <taylorzhangyx@gmail.com>
2017-06-26 14:25:50 -0700

    read type of veriables

--- tetus.ts
+++ tetus.ts
@@ -4,3 +4,11 @@ var canVote: boolean = true;
 var veriable: any = 2;
 
 document.getElementById("tsStuff").innerHTML = "My Name is " + myName;
+
+document.write("Myname is " + typeof(myName) + "</br>");
+document.write("myAge is " + typeof(myAge) + "</br>");
+document.write("canVote is " + typeof(canVote) + "</br>");
+document.write("veriable is " + typeof(veriable) + "</br>");
+
+var strToNum: number = parseInt("5");
+var numToString: number = 66;
