ad108b617b93c9a2ad4254e9445088078a361508
Yuxin Zhang <taylorzhangyx@gmail.com>
2017-06-26 15:59:20 -0700

    Add documentations and tried for loop

--- tetus.js
+++ tetus.js
@@ -1,12 +1,15 @@
+//veriable
 var myName = "Taylor";
 var myAge = 41;
 var canVote = true;
 var veriable = 2;
 document.getElementById("tsStuff").innerHTML = "My Name is " + myName;
+//read type of things
 document.write("Myname is " + typeof (myName) + "</br>");
 document.write("myAge is " + typeof (myAge) + "</br>");
 document.write("canVote is " + typeof (canVote) + "</br>");
 document.write("veriable is " + typeof (veriable) + "</br>");
+//parse number to string and verse versa
 var strToNum = parseInt("5");
 var numToString = 66;
 document.write("numToString is " + typeof (numToString.toString()) + "</br>");
@@ -17,6 +20,7 @@ var superman = {
     superName: "SuperMan"
 };
 document.write(superman.realName + " is " + superman.superName + "<br />");
+//array
 var employees = ["Bob", "Sally", "Sam"];
 document.write(employees.toString() + "<br />");
 var SuperStars = [];
@@ -25,3 +29,20 @@ SuperStars.push({
     superName: "KeyMan"
 });
 document.write(SuperStars[0].realName + " is " + SuperStars[0].superName + "<br />");
+//let veriables
+var samplet = 123;
+if (true) {
+    var samplet_1 = 456;
+}
+document.write(samplet + "<br />");
+//Array access
+var randArray = [5, 6, 7, 8];
+//for in -> out put index
+for (var val in randArray) {
+    document.write(val + "<br />");
+}
+//for of -> out put value
+for (var _i = 0, _a = randArray.map(String); _i < _a.length; _i++) {
+    var val = _a[_i];
+    document.write(val + "<br />");
+}
--- tetus.ts
+++ tetus.ts
@@ -1,3 +1,4 @@
+//veriable
 var myName: string = "Taylor";
 var myAge: number = 41;
 var canVote: boolean = true;
@@ -5,11 +6,13 @@ var veriable: any = 2;
 
 document.getElementById("tsStuff").innerHTML = "My Name is " + myName;
 
+//read type of things
 document.write("Myname is " + typeof(myName) + "</br>");
 document.write("myAge is " + typeof(myAge) + "</br>");
 document.write("canVote is " + typeof(canVote) + "</br>");
 document.write("veriable is " + typeof(veriable) + "</br>");
 
+//parse number to string and verse versa
 var strToNum: number = parseInt("5");
 var numToString: number = 66;
 document.write("numToString is " + typeof(numToString.toString()) + "</br>");
@@ -18,7 +21,7 @@ const PI : number = 3.1415926;
 
 document.write(PI.toString() + "<br />");
 
-
+//interface
 interface SuperHero {
   realName: string;
   superName: string;
@@ -31,6 +34,7 @@ var superman : SuperHero = {
 
 document.write(superman.realName + " is " + superman.superName + "<br />");
 
+//array
 var employees: string[] = ["Bob", "Sally", "Sam"];
 
 document.write(employees.toString() + "<br />");
@@ -43,3 +47,23 @@ SuperStars.push({
 });
 
 document.write(SuperStars[0].realName + " is " + SuperStars[0].superName + "<br />");
+
+//let veriables
+let samplet = 123;
+if(true){
+  let  samplet = 456;
+}
+document.write(samplet + "<br />");
+
+//Array access
+var randArray = [5,6,7,8];
+
+//for in -> out put index
+for(var val in randArray){
+  document.write(val + "<br />");
+}
+
+//for of -> out put value
+for(var val of randArray.map(String)){
+    document.write(val + "<br />");
+}
