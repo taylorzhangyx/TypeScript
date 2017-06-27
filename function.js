f144a6269de5ca4195dd666b4310df960db89bc0
Yuxin Zhang <taylorzhangyx@gmail.com>
2017-06-26 17:25:51 -0700

    function usage list

--- function.js
+++ function.js
@@ -1,5 +1,83 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var getSum = function (num1, num2) {
     return num1 + num2;
 };
 var theSum1 = getSum(5, 2);
 document.write("5 + 2 = " + theSum1 + "<br />");
+var getDiff = function (num1, num2, num3) {
+    if (num2 === void 0) { num2 = 2; }
+    if (typeof (num3) !== 'undefined') {
+        return num1 - num2 - num3;
+    }
+    return num1 - num2;
+};
+var sum2 = getDiff(5, 4, 10);
+document.write("5 - 2 = " + sum2 + "<br />");
+var sumAll = function () {
+    var nums = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        nums[_i] = arguments[_i];
+    }
+    var sum = nums.reduce(function (a, b) { return a + b; }, 0);
+    document.write("Sum: " + sum + "<br />");
+};
+sumAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+var addOne = function (x) { return x + 1; };
+document.write("1 + 3 = " + addOne(1) + "<br />");
+//Classes
+var Animal = (function () {
+    function Animal(name, owner) {
+        this.name = name;
+        this.owner = owner;
+        Animal.numOfAnimals++;
+    }
+    Animal.prototype.ownerInfo = function () {
+        document.write(this.name + " is owned by " + this.owner + "<br />");
+    };
+    Animal.howManyAnimals = function () {
+        return Animal.numOfAnimals;
+    };
+    Object.defineProperty(Animal.prototype, "weight", {
+        get: function () {
+            return this._weight;
+        },
+        set: function (weight) {
+            this._weight = weight;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Animal;
+}());
+Animal.numOfAnimals = 0;
+var spot = new Animal("Spot", "Doug");
+spot.ownerInfo();
+spot.weight = 100;
+document.write("The weight is " + spot.weight + "<br />");
+//Use static way to get static field
+document.write("# of Animals is " + Animal.howManyAnimals() + "<br />");
+var Dog = (function (_super) {
+    __extends(Dog, _super);
+    function Dog(name, owner) {
+        var _this = _super.call(this, name, owner) || this;
+        Dog.numOfAnimals++;
+        return _this;
+    }
+    return Dog;
+}(Animal));
+var grover = new Dog("Grover", "Jimmy");
+//use static way to visit static function
+document.write("# of Animals is " + Animal.howManyAnimals() + "<br />");
+//use instanceof to check the class inherient
+document.write("Is a Dog an Animal : " + (grover instanceof Animal) + "<br />");
+//in keywork to check if an property in the class
+document.write("Does grove have a name : " + ('name' in grover) + "<br />");
--- function.ts
+++ function.ts
@@ -5,3 +5,83 @@ var getSum = function(num1: number, num2: number): number{
 var theSum1: number = getSum(5, 2);
 
 document.write("5 + 2 = " + theSum1 + "<br />");
+
+var getDiff = function(num1: number, num2 = 2, num3?:number): number{
+  if(typeof(num3) !== 'undefined'){
+    return num1 - num2 - num3;
+  }
+  return num1 - num2;
+}
+
+var sum2 : number = getDiff(5,4,10);
+
+document.write("5 - 2 = " + sum2 + "<br />");
+
+
+var sumAll = function (...nums: number[]): void{
+  var sum = nums.reduce((a,b) => a+b, 0);
+  document.write("Sum: " + sum + "<br />");
+}
+
+sumAll(1,2,3,4,5,6,7,8,9,10);
+
+var addOne = (x)=>x+1;
+
+document.write("1 + 3 = " + addOne(1) + "<br />");
+
+//Classes
+class Animal {
+  public favFood: string;
+  static numOfAnimals: number = 0;
+
+  constructor(private name: string, private owner: string){
+    Animal.numOfAnimals++;
+  }
+
+  ownerInfo(){
+    document.write(this.name + " is owned by " + this.owner + "<br />");
+  }
+
+  static howManyAnimals(): number{
+    return Animal.numOfAnimals;
+  }
+
+  private _weight: number;
+
+  get weight(): number{
+    return this._weight;
+  }
+
+  set weight(weight: number){
+    this._weight = weight;
+  }
+}
+
+var spot = new Animal("Spot", "Doug");
+
+spot.ownerInfo();
+
+spot.weight = 100;
+
+document.write("The weight is " + spot.weight + "<br />");
+
+//Use static way to get static field
+document.write("# of Animals is " + Animal.howManyAnimals() + "<br />");
+
+class Dog extends Animal{
+  constructor(name: string, owner: string){
+    super(name, owner);
+    Dog.numOfAnimals++;
+  }
+}
+
+var grover = new Dog("Grover", "Jimmy");
+
+//use static way to visit static function
+document.write("# of Animals is " + Animal.howManyAnimals() + "<br />");
+
+//use instanceof to check the class inherient
+document.write("Is a Dog an Animal : " + (grover instanceof Animal) + "<br />");
+
+//in keywork to check if an property in the class
+document.write("Does grove have a name : " + ('name' in grover) + "<br />");
